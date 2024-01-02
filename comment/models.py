2b5e7822e9fcda9a415aae0c6f12f4ea364f8cef
me-ron <meronkiflayyy@gmail.com>
2024-01-02 23:29:54 +0300

    first django app

---
+++ comment/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
